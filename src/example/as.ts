3014db132bc84869be7b36563fd6ccf29cbafff5
verybeautytang <shiyu.he@happyelements.com>
2022-11-22 15:53:50 +0800

    feat: 类型推论

--- src/example/as.ts
+++ src/example/as.ts
@@ -1,1 +1,32 @@
-//  as--as 
+//  类型推论
+
+// 基础
+let a = 'sss';
+// a = 122 a会报错，这就是基础的类型推论
+
+// 多类型联合
+
+let arr4 = ['1','we',12];
+arr4 = [2,2,2,'wer']
+
+
+window.onmousedown = (mounseEvent)  =>{
+  console.log(mounseEvent)
+}
+// 上下文类型
+
+interface Info {
+  name: string
+}
+let infox:Info;
+const infoy = {name: 'xxx'}
+// 兼容性
+
+// 函数兼容性
+// 参数个数
+
+// 枚举
+
+// 类
+
+// 泛型
