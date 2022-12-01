0a590330a8e0494fb3d1438295543c366f5f4dc6
verybeautytang <shiyu.he@happyelements.com>
2022-12-01 10:59:44 +0800

    feat: add basic

---
+++ node/es6/index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+
+// console.log(fs.readFile())
+
+console.log('hello node.js')
+
+// var的缺陷
+// 声明变量的时候会有预处理【可能会造成逻辑混乱。先使用。后声明】
+// var 可以重复定义同一变量，逻辑错误，第二次应该是修改变量而不是定义
+// 在for循环中， 会造成临时变量的污染
+// var在声明的时候没有块级作用域
+
+
+// let you块级作用域  比var 更严格 存在块级作用域
